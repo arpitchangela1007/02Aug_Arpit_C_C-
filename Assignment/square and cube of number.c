ef649e112155d3be31800cec0383e8e6077385ba
Arpit R Changela <111180452+arpitchangela1007@users.noreply.github.com>
2022-08-25 17:06:08 +0530

    Add files via upload

---
+++ Assignment/square and cube of number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a, seq,cube;
+	printf("Enter number");
+	scanf("%d", &a);
+	seq=a*a;
+	cube=a*a*a;
+	printf("Square Number is: %d \n Cube Number is: %d",seq,cube);
+}
